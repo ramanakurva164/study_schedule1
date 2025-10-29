19bbbf511b8468f98846f228029b286f65bac95e
ramanakurva164 <ramana1642@gmail.com>
2025-10-29 12:17:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,8 +126,8 @@ if "plan" in st.session_state:
     st.write("#### 🗓️ Add to Google Calendar")
 
     if "google_creds" not in st.session_state:
-        client_id = st.secrets("GOOGLE_CLIENT_ID")
-        client_secret = st.secrets("GOOGLE_CLIENT_SECRET")
+        client_id = st.secrets["GOOGLE_CLIENT_ID"]
+        client_secret = st.secrets["GOOGLE_CLIENT_SECRET"]
 
         # Google OAuth flow
         flow = Flow.from_client_config(
